add1db9c96d3dd4cb4b54b910dc4936c00dcccb6
Rishu Raj <Rishurajbn654@gmail.com>
2026-03-24 04:18:35 +0530

    count character occurrences

---
+++ Char_Occurence.py
@@ -0,0 +1,12 @@
+s = input("Enter a String: ")
+
+count = {}
+
+for char in s:
+    if char in count:
+        count[char] += 1
+    else:
+        count[char] = 1
+
+for char in count:
+    print(char, "=", count[char])
