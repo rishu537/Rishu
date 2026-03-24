b0089b7a4d19ab43b14de5c29b5b6b0dab09af0b
Rishu Raj <Rishurajbn654@gmail.com>
2026-03-24 06:43:33 +0530

    String Compression

---
+++ String.Compression.py
@@ -0,0 +1,16 @@
+s = input("Enter a String: ")
+
+current_char = s[0]
+count = 1
+result = ""
+
+for char in range(1, len(s)):
+    if s[char] == current_char:
+        count += 1
+    else: 
+        result += current_char + str(count)
+        current_char = s[char]
+        count = 1
+
+result += current_char + str(count)
+print(result)
